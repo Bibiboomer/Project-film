fcb13726eb2c4a92f90f5a0d9b00e332b4539f58
Bibiboom <1321160783@qq.com>
2017-08-19 16:30:25 +0800

    传统轮播图

--- 基于粮仓/js/index.js
+++ 基于粮仓/js/index.js
@@ -39,4 +39,48 @@ $("#nav_topList li:eq(3)").mouseleave( function(){
     //输入框跳转
 
     //iBanner的轮播效果
+    //移入移出的按钮效果
+    $(".oBanners").mouseenter( function(){
+        $(".oBtns a").css("width",50);
+    })
+    $(".oBanners").mouseleave( function(){
+        $(".oBtns a").css("width",0);
+    })
+
+    //轮播效果
+    //大轮子滚动
+    //1自动播放 2鼠标移入停止播放 3 点击按钮图片切换，同时焦点改变
+    //4 点击焦点图片切换
+
+    //找到lis(图片的容器)
+    var $lis = $(".oBanners ul li");//
+    var $oBtns = $(".oBanners .oBtns");//按钮
+
+
+    //信号量
+    var idx = 0;
+    var timer = null;
+
+    //事件
+
+
+    $(".oBanners .leftBtn").click( function(){
+        //线判断当前运动状态：效果：点击一张只能动一次，不能持续运动
+        //函数节流
+        if( $lis.eq(idx).is(":animated")){
+            return;
+        }
+        //信号量改变
+        idx--;
+        //先判断
+        if( idx < 0){
+            idx = $lis.length-1;
+        }
+        $lis.eq(idx).addClass
+
+    })
     
+
+
+
+
